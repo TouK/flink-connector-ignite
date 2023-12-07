7c31dcdf52c6e272484486d5a637d77096cc3612
Pawel Czajka <pcz@touk.pl>
2023-12-07 10:47:15 +0100

    fix

--- src/test/java/pl/touk/flink/ignite/StreamTableEnvironmentUtil.java
+++ src/test/java/pl/touk/flink/ignite/StreamTableEnvironmentUtil.java
@@ -1,10 +1,8 @@
 package pl.touk.flink.ignite;
 
 import org.apache.flink.api.common.RuntimeExecutionMode;
-import org.apache.flink.configuration.ExecutionOptions;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
-import org.apache.flink.table.api.TableConfig;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.table.api.bridge.java.internal.StreamTableEnvironmentImpl;
 
@@ -13,11 +11,9 @@ public class StreamTableEnvironmentUtil {
     public static StreamTableEnvironment create() {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setRuntimeMode(RuntimeExecutionMode.BATCH);
-        TableConfig tableConfig = new TableConfig();
-        tableConfig.getConfiguration().set(ExecutionOptions.RUNTIME_MODE, RuntimeExecutionMode.BATCH);
         EnvironmentSettings settings = EnvironmentSettings.newInstance().inBatchMode().build();
 
-        return StreamTableEnvironmentImpl.create(env, settings, tableConfig);
+        return StreamTableEnvironmentImpl.create(env, settings);
     }
 
 }
