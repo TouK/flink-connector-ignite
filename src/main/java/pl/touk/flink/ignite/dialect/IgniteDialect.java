b4b16d6174350e496e3a45512fe45b42d8243601
Paweł Czajka <paw787878@gmail.com>
2023-12-15 13:09:21 +0100

    Bump flink to 1 16 (#10)
    * set flink version to 1.16
    
    * dependency renames
    
    * set scala version to 2.12
    
    * fix
    
    * fix
    
    * fix
    
    * nothing is changed
    
    * now it works
    
    * used abstract base class
    
    * pr fixes
    
    * comment added
    
    * MAX_TIMESTAMP_PRECISION changed
    
    * removed copied from postgressDialect comments
    
    ---------
    
    Co-authored-by: Pawel Czajka <pcz@touk.pl>

--- src/main/java/pl/touk/flink/ignite/dialect/IgniteDialect.java
+++ src/main/java/pl/touk/flink/ignite/dialect/IgniteDialect.java
@@ -1,24 +1,29 @@
 package pl.touk.flink.ignite.dialect;
 
-import org.apache.flink.connector.jdbc.dialect.JdbcDialect;
-import org.apache.flink.connector.jdbc.internal.converter.JdbcRowConverter;
+import org.apache.flink.connector.jdbc.converter.JdbcRowConverter;
+import org.apache.flink.connector.jdbc.dialect.AbstractDialect;
+import org.apache.flink.table.types.logical.LogicalTypeRoot;
 import org.apache.flink.table.types.logical.RowType;
 import pl.touk.flink.ignite.converter.IgniteRowConverter;
 
+import java.util.EnumSet;
 import java.util.Optional;
+import java.util.Set;
 
-public class IgniteDialect implements JdbcDialect {
-
+// TODO_PAWEL jest problem, jak sie zamieni wszystkie metody na throw runtime exception oprocz quoteIdentifier i getRowConverter to
+// testy dalej przechodza
+public class IgniteDialect extends AbstractDialect {
     private static final long serialVersionUID = 1L;
+
+    // value chosen based on https://ignite.apache.org/docs/latest/sql-reference/data-types#timestamp
+    private static final int MAX_TIMESTAMP_PRECISION = 9;
+    private static final int MIN_TIMESTAMP_PRECISION = 1;
+    private static final int MAX_DECIMAL_PRECISION = 1000;
+    private static final int MIN_DECIMAL_PRECISION = 1;
 
     @Override
     public String dialectName() {
         return "Ignite";
-    }
-
-    @Override
-    public boolean canHandle(String url) {
-        return url.startsWith("jdbc:ignite:thin");
     }
 
     @Override
@@ -32,8 +37,50 @@ public class IgniteDialect implements JdbcDialect {
     }
 
     @Override
+    public String quoteIdentifier(String identifier) {
+        return "\"" + identifier + "\"";
+    }
+
+    @Override
     public Optional<String> defaultDriverName() {
         return Optional.of("org.apache.ignite.IgniteJdbcThinDriver");
     }
 
+    // not supported for now, probably merge can be used https://ignite.apache.org/docs/latest/sql-reference/dml#merge
+    @Override
+    public Optional<String> getUpsertStatement(
+            String tableName, String[] fieldNames, String[] uniqueKeyFields) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Range> decimalPrecisionRange() {
+        return Optional.of(Range.of(MIN_DECIMAL_PRECISION, MAX_DECIMAL_PRECISION));
+    }
+
+    @Override
+    public Optional<Range> timestampPrecisionRange() {
+        return Optional.of(Range.of(MIN_TIMESTAMP_PRECISION, MAX_TIMESTAMP_PRECISION));
+    }
+
+    @Override
+    public Set<LogicalTypeRoot> supportedTypes() {
+        return EnumSet.of(
+                LogicalTypeRoot.CHAR,
+                LogicalTypeRoot.VARCHAR,
+                LogicalTypeRoot.BOOLEAN,
+                LogicalTypeRoot.VARBINARY,
+                LogicalTypeRoot.DECIMAL,
+                LogicalTypeRoot.TINYINT,
+                LogicalTypeRoot.SMALLINT,
+                LogicalTypeRoot.INTEGER,
+                LogicalTypeRoot.BIGINT,
+                LogicalTypeRoot.FLOAT,
+                LogicalTypeRoot.DOUBLE,
+                LogicalTypeRoot.DATE,
+                LogicalTypeRoot.TIME_WITHOUT_TIME_ZONE,
+                LogicalTypeRoot.TIMESTAMP_WITHOUT_TIME_ZONE,
+                LogicalTypeRoot.TIMESTAMP_WITH_LOCAL_TIME_ZONE,
+                LogicalTypeRoot.ARRAY);
+    }
 }
